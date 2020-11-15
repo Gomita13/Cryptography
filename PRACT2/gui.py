4cacb2e0222833272457fc767b9904de9a31abbe
Aarón Gama <25625929+ElWakamolexD@users.noreply.github.com>
2020-11-14 20:28:52 -0600

    v.2.1
    Ya descifra imágenes.

--- PRACT2/gui.py
+++ PRACT2/gui.py
@@ -5,6 +5,13 @@ from tkinter.filedialog import askopenfilename
 import sys
 from Crypto.Cipher import DES
 
+def selectIVFile(event):
+	global ivFile
+	global lblIV
+	ivFile = askopenfilename()
+	lblIV["text"] = "SELECTED IV FILE:\n"+ivFile
+	ivFile = open(ivFile,"rb").read() #LEEMOS LOS BYTES DEL ARCHIVO DE UNA VEZ
+
 def selectFile(event):
 	global file
 	global lblImg
@@ -36,14 +43,40 @@ def encryptImage(event):
 	file_iv.write(cipher.iv)
 	file_out.close()
 	file_iv.close()
+	file.close()
 
+def decryptImage(event):
+	global file
+	global ivFile
+	global cbMode
+	global txtKey
+	if ivFile!=None:
+		key = str.encode(txtKey.get())
+		mode = str(cbMode.get())
+		if mode == 'ECB':
+			cipher = DES.new(key,DES.MODE_ECB)
+		elif mode == 'CBC':
+			cipher = DES.new(key,DES.MODE_CBC,ivFile)
+		elif mode == 'CFB':
+			cipher = DES.new(key,DES.MODE_CFB,ivFile)
+		else:
+			cipher = DES.new(key,DES.MODE_OFB,ivFile)
+		file_out = open(file.name[:-8]+".bmp","wb")
+		header = file.read(54)
+		file_out.write(header) #IMAGE HEADER
+		content = file.read()
+		file_out.write(cipher.decrypt(content))
+		file_out.close()
+		file.close()
+	else:
+		print('Select an IV file in order to decode your image')
 #CREAMOS LA VENTANA Y ASIGNAMOS UN TAMAÑO
 window = tk.Tk()
 window.title("DES IMAGE ENCRYPTION")
-frame = tk.Frame(master=window,width=700,height=620,bg="black")
+frame = tk.Frame(master=window,width=590,height=620,bg="black")
 
 #CREAMOS UN LABEL PARA VISUALIZAR LA IMAGEN
-lblImg = tk.Label(foreground="medium purple",background="black",height=30,width=82,font=("Courier",10),anchor="w")
+lblImg = tk.Label(foreground="medium purple",background="red",height=30,width=68,font=("Courier",10),anchor="w")
 lblImg.place(x=20,y=20)
 
 #CREAMOS EL BOTÓN PAR EL ARCHIVO
@@ -68,16 +101,29 @@ cbMode = ttk.Combobox(values=["ECB","CBC","CFB","OFB"])
 cbMode.current(0)
 cbMode.place(x=300,y=520)
 
+#CREAMOS UN BOTON PARA EL ARCHIVO IV
+ivBtn = tk.Button(text="LOAD IV FILE",width=12,height=1,fg="hot pink",bg="black")
+ivBtn.bind("<Button-1>",selectIVFile)
+ivBtn.place(x=470,y=520)
+
 #CREAMOS EL BOTÓN DE ENCRYPT Y DECRYPT
 encbtn = tk.Button(text="ENCRYPT",width=10,height=1,fg="hot pink",bg="black")
 encbtn.bind("<Button-1>",encryptImage)
 encbtn.place(x=20,y=570)
 decbtn = tk.Button(text="DECRYPT",width=10,height=1,fg="hot pink",bg="black")
 decbtn.place(x=110,y=570)
+decbtn.bind("<Button-1>",decryptImage)
+
+#CREAMOS UN LBL PARA INDICAR EL STATUS DEL ARCHIVO DEL IV
+lblIV = tk.Label(text="IV STATUS:\nNOT SELECTED",foreground="medium purple",background="black",height=3,font=("Courier",10),anchor="w")
+lblIV.place(x=220,y=550)
 
 frame.pack()
 file = None
+ivFile = None
 try:
 	window.mainloop()
 except KeyboardInterrupt:
 	sys.exit()
+except AttributeError:
+	pass
